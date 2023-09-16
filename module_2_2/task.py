879466e4486fa4e7cc9e3d86c8d92e52f8905085
hotbloodseo <hotbloodseo@gmail.com>
2023-09-16 22:55:47 +0300

    wtf 1

---
+++ module_2_2/task.py
@@ -0,0 +1,8 @@
+is_active = bool(input("Is the user active? "))
+is_admin = bool(input("Is the user administrator? "))
+is_permission = bool(input("Does the user have access? "))
+
+access=True
+is_active=True 
+is_admin=True 
+is_permission=True

0f5004243f9484b42866bacf887bc37f7fc384fc
hotbloodseo <hotbloodseo@gmail.com>
2023-09-18 00:04:50 +0300

    wtf

---
+++ module_2_8/task.py
@@ -0,0 +1,5 @@
+message = "Never argue with stupid people, they will drag you down to their level and then beat you with experience."
+search = "r"
+result = 0
+for result in message:
+    print("",result)
